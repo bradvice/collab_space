c7616db186afdb4e0418ca123e94fc4daa624aef
Eric Stautmeister <eric@stautmeister.ch>
2021-09-15 09:04:50 +0200

    ejs integration

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.disable("x-powered-by");
+
+const port = 3000;
+const site = `http://localhost:${port}`
+
+app.use(express.static('public'))
+app.use('/src', express.static(`${__dirname}/public/src`))
+app.use('/views', express.static(`${__dirname}/public/views`))
+app.use('/js', express.static(`${__dirname}/public/src/js`))
+app.use('/css', express.static(`${__dirname}/public/src/css`))
+
+app.set('views', './public/views')
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res) => {
+    await res.render(`index`)
+});
+
+app.listen(port, () => console.info(`App available on ${site}`))
