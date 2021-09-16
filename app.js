df5125252105c9c8a49a8fb9889a0983c0ee044b
bradvice <merlinminder1@gmail.com>
2021-09-16 11:36:51 +0200

    -

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.disable("x-powered-by");
 
 const port = 3000;
-const site = `http://localhost:${port}`
+const site = `http://localhost:${port}`;
 
-app.use(express.static('public'))
-app.use('/views', express.static(`${__dirname}/public/views`))
-app.use('/src', express.static(`${__dirname}/public/src`))
-app.use('/js', express.static(`${__dirname}/public/src/js`))
-app.use('/css', express.static(`${__dirname}/public/src/css`))
+app.use(express.static("public"));
+app.use("/views", express.static(`${__dirname}/public/views`));
+app.use("/src", express.static(`${__dirname}/public/src`));
+app.use("/js", express.static(`${__dirname}/public/src/js`));
+app.use("/css", express.static(`${__dirname}/public/src/css`));
 
-app.set('views', './public/views')
-app.set('view engine', 'ejs')
+app.set("views", "./public/views");
+app.set("view engine", "ejs");
 
-app.get('/', async (req, res) => {
-    await res.render(`index`)
+app.get("/", async (req, res) => {
+  await res.render(`index`);
 });
 
-app.get('/login', async (req, res) => {
-    await res.render('login')
+app.get("/login", async (req, res) => {
+  await res.render("login");
 });
 
-app.get('/signup', async (req, res) => {
-    await res.render('signup')
+app.get("/signup", async (req, res) => {
+  await res.render("signup");
 });
 
-app.post('/signup', async (req, res) => {
-    console.log(req.body)
+app.get("/timer", async (req, res) => {
+  await res.render("timer");
 });
 
-app.post('/login', async (req, res) => {
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
 });
 
-app.listen(port, () => console.info(`App available on ${site}`))
+app.post("/login", async (req, res) => {});
+
+app.listen(port, () => console.info(`App available on ${site}`));
